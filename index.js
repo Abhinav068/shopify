92a7323a133e4c70d9a6e2b0ef7b73aa2a02a2b9
Abhinav068 <abhinav28897@gmail.com>
2024-04-08 10:41:13 +0530

    connecting to spotify

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Shopify = require('shopify-api-node');
 
 const app = express();
 require('dotenv').config();
@@ -7,6 +8,22 @@ const PORT = 8000;
 app.get('/shopify', (req, res) => {
     res.sendFile(__dirname + '/view/test.html')
 })
+
+
+const shopify = new Shopify({
+  shopName: 'abhinav-store',
+  apiKey: process.env.SHOPIFY_API_KEY,
+  password: process.env.SHOPIFY_API_SECRET
+});
+
+
+shopify.on('callLimits', (limits) => console.log(limits));
+
+shopify.order
+  .list({ limit: 5 })
+  .then((orders) => console.log(orders))
+  .catch((err) => console.error(err));
+
 
 
 app.listen(PORT, () => {
